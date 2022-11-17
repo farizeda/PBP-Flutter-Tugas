3d6fa7dbbe12ece26053e33c425937bba05b0408
farizedaandhika21 <farizedaandhika21@gmail.com>
2022-11-17 11:28:32 +0700

    Tugas 8

--- README.md
+++ README.md
@@ -41,3 +41,53 @@ sehingga tampilan dapat mencerminkan nilai yang diperbarui. Jika kita berubah _c
 Bonus:
 - Arah Row diubah dari kanan ke kiri sehingga kemunculan pertama adalah tombol (+), lalu (-) 
 - Terapkan conditional tombol (-) untuk tidak dapat menghilang
+
+
+
+## **Lab 8**
+### 1. Perbedaan `Navigator.push` dan `Navigator.pushReplacement`
+`Navigator.push` : menambahkan `Route` ke *top of stack* dari `Navigator`.
+`Navigator.pushReplacement`:  menambahkan route ke *top of stack* juga menghilangkan `Route` yang sebelumnya.
+
+### 2. *Widget* yang Digunakan
+* Text : Menampilkan sebuah *string* dengan *single style*.
+* Column: Sebuah *widget* yang menampilkan *children*-nya secara vertikal.
+* Row : Sebuah *widget* yang menampilkan *children*-nya secara horizontal.
+* Scaffold : Menerapkan dasar dari *visual layout* Material Design.
+* AppBar : *App bar* dari Material Design
+* Padding : Memberi *padding* pada *child* yang diletakkan di dalamnya.
+* Center : Sebuah *widget* yang *child*-nya akan berposisi di tengah.
+* Drawer: Panel yang berdiri secara horizontal di pinggir `Scaffold` yang menampilkan tautan navigasi dalam aplikasi.
+*  ListTile : Sebuah baris yang biasanya mengandung teks dan ikon.
+* Navigator : Mengelola *child widgets* menggunakan prinsip *stack*.
+* MaterialPageRoute: Rute modal yang menggantikan seluruh layar dengan transisi platform-adaptif.
+* SingleChildScrollView : Digunakan agar *child*-nya bisa di-*scroll*.
+* Container : *Widget* yang menggabungkan *widget* umum untuk *painting*, *positioning*, dan *sizing*.
+* Card: Sebuah panel yang bisa digunakan untuk menampilkan informasi di dalamnya. Memiliki *style* dimana sudutnya sedikit bulat dan terdapat bayangan elevasi.
+* CustomScrollView : Kurang lebih sama dengan `SingleChildScrollView` namun lebih fleksibel.
+* SliverFillRemainingFill: Sebuah *sliver* yang mengandung sebuah *box child* yang mengisi sisa ruang pada *viewport*.
+* Form: *Containter* untuk melakukan *grouping* terhadap beberapa *form field widget*.
+* TextFormField: *Field* teks yang bisa digunakan untuk melakukan *input*.
+* InputDecoration: Melakukan dekorasi pada sebuah *input field*.
+* DropdownButtonFormField: Menu *dropdown*.
+* Expanded: Melebarkan *child* dari `Row`, `Column`, atau `Flex` agar mengisi ruang yang masih tersedia.
+* Align: Melakukan *aligining* terhadap *child*-nya.
+* TextButton : Tombol yang berisi teks.
+
+### 3. Jenis-jenis *event* pada FLutter
+1. onPressed        : Terpanggil saat button ditekan
+2. onLongPress      : Terpanggil saat button ditekan lama
+3. onTap            : Terpanggil saat salah satu popup menu ditekan dan akan menjalankan actions yang telah ditentukan
+4. onFocusChanged   : Terpanggil saat focus berganti
+5. onSaved          : Terpanggil dengan final value saat form telah di save via FormState
+6. onHover          : Terpanggil saat pointer masuk ke button response area
+7. onChanged        : Terpanggil saat user meninisiasi sebuah perubahan kepada value TextField
+
+### 4. Cara kerja `Navigator`
+`Navigator` menggunakan prinsip stack dalam mengganti halaman. Halaman yang ditampilkan adalah yang berada pada top of stack.
+
+### 5. Implementasi
+* Membuat *form* beserta *input field* yang dinginkan mengacu pada tutorial.
+* Data-data yang di-input disimpan kedalam sebuah `List`.
+* Data yang disimpan berupa `Card`.
+* Tampilkan data pada halaman Data Budget sesuai dengan yang sudah ditambahkan dengan memanfaatkan `List` yang tadi sudah dibuat.
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+
+import 'package:tugas/form_budget.dart';
+import 'package:tugas/data_budget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,7 +27,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: "Program Counter"),
+      home: const MyHomePage(title: 'Program Counter'),
     );
   }
 }
@@ -63,84 +66,121 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _decrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      if (_counter > 0) {
-        _counter--;
-      }
+      _counter--;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter and _decrementCounter method above.
+    // by the _incrementCounter method above.
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            // Menambahkan clickable menu
+            ListTile(
+              title: const Text('counter_7'),
+              onTap: () {
+                // Route menu ke halaman utama
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Program Counter')),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Tambah Budget'),
+              onTap: () {
+                // Route menu ke halaman tambah budget
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const AddBudgetPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Data Budget'),
+              onTap: () {
+                // Route menu ke halaman data budget
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const DataBudgetPage()),
+                );
+              },
+            ),
+          ],
         ),
-        // This trailing comma makes auto-formatting nicer for build methods.
-        body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              if (_counter % 2 != 0) ...[
-                const Text("GANJIL", style: TextStyle(color: Colors.blue)),
-              ] else ...[
-                const Text("GENAP", style: TextStyle(color: Colors.red)),
-              ],
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
-          ),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: _counter & 1 == 0
+              ? <Widget>[
+            const Text(
+              'Genap',
+              style: TextStyle(color: Colors.blue),
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ]
+              : <Widget>[
+            const Text(
+              'Ganjil',
+              style: TextStyle(color: Colors.red),
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
         ),
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.only(left: 35),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            textDirection: TextDirection.rtl,
-            children: [
-              FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: const Icon(Icons.add),
+      ),
+      floatingActionButton: Padding(
+          padding: const EdgeInsets.only(left: 30),
+          child:
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Visibility(
+              child: FloatingActionButton(
+                onPressed: _decrementCounter,
+                tooltip: 'Decrement',
+                child: const Icon(Icons.remove),
               ),
-              if (_counter > 0) ...[
-                FloatingActionButton(
-                  onPressed: _decrementCounter,
-                  tooltip: 'Decrement',
-                  child: const Icon(Icons.remove),
-                ),
-              ]
-            ],
-          ),
-        ));
+              visible: _counter != 0,
+            ),
+            FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ),
+          ])), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
